5c889a5742e2c9faca98f1b38663f44a3e85b9bb
NataliaMujicaH <natalia.mujicah@gmail.com>
2021-09-27 23:18:37 -0300

    actualizacion class Travel

--- home/models.py
+++ home/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 from login.models import User
 
+class TravelManager(models.Manager):
+    def basic_validator(self, postData):
+        errores = {}
+        if len(postData['destination']) == 0:
+            errores['destination'] = "Destination is required"
+        if len(postData['description']) == 0:
+            errores['description'] = "Description is required"
+        if len(postData['df']) == 0:
+            errores['df'] = "Start date is required"
+        if len(postData['dt']) == 0:
+            errores['dt'] = "End date is required"
+        return errores
+
 # Create your models here.
 class Travel(models.Model):
-    planned_by = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    planned_by = models.ForeignKey(User, related_name='planner')
     destination = models.CharField(max_length=40)
     description = models.CharField(max_length=300)
     df = models.DateTimeField() # date from
     dt = models.DateTimeField() # date to
-    join = models.ManyToManyField(User, through='Join')
+    join_user = models.ForeignKey(User, related_name='joining_user')
 
-class Join(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    travel_id = models.ForeignKey(Travel, on_delete=models.CASCADE)
-    join_at = models.DateTimeField(auto_now_add=True)
+
  
